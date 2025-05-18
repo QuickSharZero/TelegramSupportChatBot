bc6f6bb01c466bbc6af95c4d1fff68f3829de516
QuickShar <slava44400@gmail.com>
2025-05-18 19:00:21 +1000

    Добавлена поддержка плейсхолдеров

--- modules/message_handler.py
+++ modules/message_handler.py
@@ -1,15 +1,28 @@
 import yaml
 from pathlib import Path
+from aiogram.types import Message
+import re
+from typing import Optional
 
 
 class MessageHandler:
 
     default_messages = {
+        # Сообщения поддерживают форматирование Markdown и плейсхолдеры:
+        # {username}, {user_id}, {user_first_name}, {user_last_name},
+        # {chat_id}, {chat_is_forum}, {thread_id}
         "start": {
             "greeting": "Привет! \nНапиши мне сообщения и я отправлю его техподдержке."
         },
         "reload_messages": {
-            "succes": "Сообщения бота успешно перезагружены"
+            "succes": "Сообщения бота успешно перезагружены",
+            "reload": "Не удалось обновить сообщения бота"
+        },
+
+        "get_chat_info": "ID чата: `{chat_id}`\nЭто форум?: {chat_is_forum}\nThread ID: `{thread_id}`",
+
+        "errors": {
+            "NoPerm": "Вы не можете использовать данную команду"
         }
     }
 
@@ -29,11 +42,25 @@ class MessageHandler:
     async def reload(self):
         self.messages = self.load_messages()
 
-    async def get(self, *keys):
-        value = self.messages
-        for key in keys:
-            value = value.get(key, {})
-        return value
+    async def get(self, key: str, message: Optional[Message] = None):
+        messages = self.messages
+        keys = key.split('.')
+        for k in keys:
+            messages = messages.get(k, {})
+
+        context = {}
+        if message:
+            context.update({
+                'username': message.from_user.username,
+                'user_id': message.from_user.id,
+                'user_first_name': message.from_user.first_name,
+                'user_last_name': message.from_user.last_name,
+                'chat_id': message.chat.id,
+                'chat_is_forum': message.chat.is_forum,
+                'thread_id': message.message_thread_id
+            })
+
+        return messages.format(**context)
 
     def _isFileExists(self):
         try:
